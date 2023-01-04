d5e75a9f3c08d1db01b289fce72d28d7522dde74
Sayuk <43848737+Sayuk@users.noreply.github.com>
2023-01-04 18:42:34 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+console.log(SimpleLightbox);
